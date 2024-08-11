a0c7958586167d599991d1b67c9df3569d013c1f
edohwares <edohemmanuel4real@gmail.com>
2024-08-11 22:35:29 +0100

    added react-loader-spinner

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,24 +6,49 @@ import AdminAuth from './pages/Admin/AdminAuth';
 import AdminBoard from './pages/Admin/AdminBoard';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useEffect, useState } from 'react';
+import {ThreeCircles} from 'react-loader-spinner';
 
 function App() {
 
+  const [loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+  }, []);
 
   return (
-    <section className='max-w-[1440px] mx-auto'>
-      <ToastContainer />
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='auth' element={<Auth />} />
-          <Route path='admin-auth' element={<AdminAuth />} />
-          <Route path='admin' element={<AdminBoard />} />
-          <Route path='*' />
-        </Routes>
-      </BrowserRouter>
-    </section>
+    <>
+      {
+        loading ? 
+        <div className='w-screen h-screen flex items-center justify-center'>
+          <ThreeCircles
+            visible={true}
+            height="100"
+            width="100"
+            color="#4fa94d"
+            ariaLabel="three-circles-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+        />
+        </div>
+        :
+        <section className='max-w-[1440px] mx-auto'>
+        <ToastContainer />
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='auth' element={<Auth />} />
+            <Route path='admin-auth' element={<AdminAuth />} />
+            <Route path='admin' element={<AdminBoard />} />
+            <Route path='*' />
+          </Routes>
+        </BrowserRouter>
+      </section>}
+    </>
   )
 }
 
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import logo from '../../assets/images/logo.png';
 import './Header.css';
 import { TiThMenuOutline } from "react-icons/ti";
+import { Link } from 'react-router-dom';
 
 
 const Header = () => {
@@ -17,11 +18,11 @@ const Header = () => {
   return (
     <div className='absolute top-0 right-0 left-0 '>
       <div className="content flex items-center justify-between pl-[-1rem] md:pl-0 pr-4 md:pr-10 relative md:h-[8rem]">
-        <div className="logo md:w-[20%]">
+        <Link to={'/'} className="logo md:w-[20%] cursor-pointer">
             <img src={logo} alt="logo" 
                 className='w-[130px] md:w-[144px]'
             />
-        </div>
+        </Link>
         <nav className='md:w-[80%] w-full absolute md:static right-0'>
             <ul className={`flex flex-col ${!showMobileNav ? 'hidden' : ''} md:flex md:flex-row items-center space-y-4 md:space-y-0 mt-10 md:mt-0 bg-white shadow-md md:shadow-[0] md:bg-transparent md:space-x-4 justify-between w-[50%] h-[70vh] md:w-full absolute md:static top-0 right-0 py-6 md:py-0`}>
                 <a onClick={() => handleNavClick('/')} href={'/'} className={`${activeNav == '/' ? 'active-nav' : ''} cursor-pointer text-sm`}>Home</a>
