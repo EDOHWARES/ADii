b15780c95a8579d20c531c7ff7a9aa9c2ba2a19a
EDOHWARES <edohemmanuel4real@gmail.com>
2024-08-25 02:31:28 +0100

    edited the intro text

--- frontend/src/components/Texts/Texts.jsx
+++ frontend/src/components/Texts/Texts.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
 
 const Texts = () => {
   return (
     <div className='flex flex-col gap-[1rem] px-4 md:px-10 text-[#4C4C4C] leading-[27px] mb-[3rem]'>
-      <p className='text-justify text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, corrupti, eveniet ipsam ut sequi harum, provident cumque perspiciatis itaque ratione laudantium beatae natus pariatur rem expedita dolor eum adipisci magni.</p>
-      <p className='text-justify text-sm'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque, eligendi! Iure quod rerum sint nesciunt quasi sequi soluta totam. Consequatur sunt alias architecto officiis deserunt sapiente odio voluptatum soluta! Dolorem.</p>
-      <p className='text-justify text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur mollitia eligendi ipsam accusamus earum tenetur ad dolor illum consequatur impedit nulla odio natus, magnam corrupti illo voluptate soluta beatae rerum?</p>
+      <p className='text-justify text-sm'>Welcome to Adiinsight a not-for-profit initiative dedicated to tracking and analyzing the prices of essential goods and services across Nigeria&apos;s six(6) geographical zones. Our mission is to provide insights into the impact of economic reforms, regional differences, religious influences, and other factors on the cost of living in various states and zones.</p>
+      <div>
+        <p className='text-justify text-sm'>We collect and update data twice daily on:</p>
+        <ul className="list-disc ml-[2rem]">
+          <li>Food prices</li>
+          <li>Rent and accommodation costs</li>
+          <li>Petrol and fuel prices</li>
+          <li>And other essential expenses</li>
+        </ul>
+      </div>
+
+      <div>
+        <p className='text-justify text-sm'>Our unique approach combines human data collection with machine learning algorithms to ensure accuracy and reliability. By doing so, we aim to:</p>
+        <ul className="list-disc ml-[2rem]">
+          <li>Inform policy decisions with data-driven insights</li>
+          <li>Enhance economic understanding and research</li>
+          <li>Support businesses and individuals in making informed decisions</li>
+          <li>Foster transparency and accountability in price setting</li>
+        </ul>
+      </div>
+
+      <p>Explore our website to access up-to-date price data, interactive dashboards, and in-depth analysis. Together, let&apos;s work towards a more equitable and prosperous Nigeria.</p>
+
+      <p><b>Note:</b> We are a not-for-profit organisation, and our data is available for free to ensure equal access to information for all.</p>
     </div>
   )
 }
