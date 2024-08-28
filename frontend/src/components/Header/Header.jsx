eb81860743495d30fbf9e642a639b29ba2bda4ee
EDOHWARES <edohemmanuel4real@gmail.com>
2024-08-28 13:10:49 +0100

    removed the nav breadcrumb

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -10,15 +10,10 @@ import Modal from '../Modal/Modal';
 const Header = () => {
 
   const [showMobileNav, setShowMobileNav] = useState(false);
-  const [activeNav, setActiveNav] = useState('/');
 
   const [openModal, setOpenModal] = useState(false);
 
   // handle nav click
-  const handleNavClick = (nav) => {
-    setShowMobileNav(false);
-    setActiveNav(nav);
-  };
 
   return (
     <div className='absolute top-0 right-0 left-0 max-w-[1600px] mx-auto'>
@@ -30,11 +25,11 @@ const Header = () => {
         </Link>
         <nav className='md:w-[80%] w-full absolute md:static right-0'>
             <ul className={`flex flex-col ${!showMobileNav ? 'hidden' : ''} md:flex md:flex-row items-center space-y-4 md:space-y-0 mt-10 md:mt-0 bg-white shadow-md md:shadow-[0] md:bg-transparent md:space-x-4 justify-between w-[50%] h-[35vh] md:w-full absolute md:static top-0 right-0 py-6 md:py-0`}>
-                <a onClick={() => handleNavClick('/')} href={'/'} className={`${activeNav == '/' ? 'active-nav' : ''} cursor-pointer md:text-sm text-[13px]`}>Home</a>
-                <a onClick={() => handleNavClick('#market')} href={'#market'} className={`${activeNav == '#market' ? 'active-nav' : ''} cursor-pointer md:text-sm text-[13px]`}>Markets</a>
-                <Link to={'/about'} onClick={() => handleNavClick('/about')} className={`${activeNav == '/about' ? 'active-nav' : ''} cursor-pointer md:text-sm text-[13px]`}>About Us</Link>
-                <a onClick={() => handleNavClick('#footer')} href={'#footer'} className={`${activeNav == '#footer' ? 'active-nav' : ''} cursor-pointer md:text-sm text-[13px]`}>Contact Us</a>
-                <a onClick={() => handleNavClick('#newletter')} href={'#newsletter'}>
+                <a href={'/'} className={`cursor-pointer md:text-sm text-[13px]`}>Home</a>
+                <a href={'#market'} className={`cursor-pointer md:text-sm text-[13px]`}>Markets</a>
+                <Link to={'/about'} className={`cursor-pointer md:text-sm text-[13px]`}>About Us</Link>
+                <a href={'#footer'} className={`cursor-pointer md:text-sm text-[13px]`}>Contact Us</a>
+                <a href={'#footer'}>
                     <button className='border md:text-sm text-[13px] border-[#276100] text-[#276100] hover:bg-[#276100] hover:text-white duration-500 px-4 md:px-6 py-2 md:py-4 rounded-[9px]'>Join Newsletter</button>
                 </a>
                 <li>
